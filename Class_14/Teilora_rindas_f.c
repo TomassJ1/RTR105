755e32c3d02e269cbf6d1cd52fc73a3ef167c0d0
TomassJ1 <tomasssjevdokimovs@inbox.lv>
2023-11-13 21:53:58 +0200

    Class_14_15

--- Class_14/Teilora_rindas_f.c
+++ Class_14/Teilora_rindas_f.c
@@ -1,17 +1,16 @@
 #include<stdio.h>
 #include<math.h>
-void main(void)
+
+
+
+double mans_sinuss(double x)
 {
-double x=2.05, y, a, S;
+double a,S;
 int k=0;
-y=sin(x);
-printf("y=sin(%.2f)=%.2f\n", x, y);
 
 a = pow(-1,k)*pow(x,2*k+1)/(1.);
 S = a;
 printf("%.2f\t%8.2f\t%8.2f\n", x,a, S);
-
-
 
 while(k<3)
 {
@@ -20,5 +19,16 @@ a = a* (-1)*x*x/ ((2*k)*(2*k+1));
 S = S + a;
 printf("%.2f\t%8.2f\t%8.2f\n", x,a, S);
 }
-
+return S;
 }
+
+void main()
+{
+double x=2.05,y,yy;
+y=sin(x);
+printf("standarta funkcija sin - y=sin(%.2f)=%.2f\n",x,y);
+
+yy = mans_sinuss(x);
+printf("lietotaaja funkcija - y=mans_sinuss(%.2f)=%.2f\n",x,yy);
+}
+
