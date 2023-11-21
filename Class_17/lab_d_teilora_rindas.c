03ac49f9a379abed1c6f3cdb43749b96931f84cd
TomassJ1 <tomasssjevdokimovs@inbox.lv>
2023-11-21 11:56:16 +0200

    Class_17_18

---
+++ Class_17/lab_d_teilora_rindas.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<math.h>
+
+double eksponensionala_funkcija(double x);
+
+
+void main()
+{
+    double x,y,yy;
+    printf("Funkcijas (1+x)*e^x aprēķināšana:\n");
+    printf("Ievadi argumentu x:  ");
+    scanf("%lf",&x);
+    y=(1+x)*exp(x);
+    
+    yy = eksponensionala_funkcija(x);
+    printf("(1+%.2f)*e^(%.2f) caur summu: %.5f\n\n",x,x,yy);
+
+    printf("                  1000\n");
+    printf("                  _____\n");
+    printf("                  \\               k\n");
+    printf("                   \\       (1+x)*x\n");
+    printf("(1+%.2f)*e^(%.2f) = > _______________________\n", x,x);
+    printf("                   /\n");
+    printf("                  /_____      k!\n");
+    printf("                    k=0   \n\n\n\n");
+
+    printf("                                   k\n");
+    printf("                            (1+x)*x\n");
+    printf("rekurences reizinātājs:   ________________\n");
+    printf("                                 k!\n\n\n");
+
+
+
+
+}
+
+double eksponensionala_funkcija(double x)
+{
+    double a,S;
+    int k=0;
+
+    a = (1.+x);
+    S = a;
+   
+    printf("a%d = %.2f;  S%d = %.2f\n",k,a,k,S);
+    while(k<1000)
+    {
+        k++;
+        a = (x*a)/k;
+        S = S + a;
+
+        if (k==999)
+        {
+            printf("a%d = %.2f;  S%d = %.2f\n",k,a,k,S);
+        }
+        if (k==1000)
+        {
+            printf("a%d = %.2f;  S%d = %.2f\n",k,a,k,S);
+        }
+
+    }
+
+
+    return S;
+}
