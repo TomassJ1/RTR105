da2cd4d75f29eeea5dc9585bb9c4e98d297d9729
TomassJ1 <tomasssjevdokimovs@inbox.lv>
2023-12-19 11:46:57 +0200

    Class_22

--- Class_20/skaitliska_integresana_2.c
+++ Class_20/skaitliska_integresana_2.c
@@ -8,6 +8,14 @@ void main()
     int k, n=2;
     float integr1=2*eps;
 
+    printf("Ievadiet a vērtību:");
+    scanf("%f",&a);
+    printf("Ievadiet b vērtību:");
+    scanf("%f",&a);
+    printf("Ievadiet precizitāti:");
+    scanf("%f",&a);
+
+
     integr2=(b-a)*((1+a)*exp(a)+(1+b)*exp(b))/n;
 
     while(fabs(integr2-integr1)>eps)
